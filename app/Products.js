8cd2b6762ced82d5c1d9a40a18281016f2d87d08
KunalHydrogen <kunal@binaryic.co.in>
2024-02-09 18:15:17 +0530

    product Featured Collection

---
+++ app/Products.js
@@ -0,0 +1,31 @@
+export const Sony_data =  [
+      {
+        "title": "Sony Smart TV",
+        "price": "$799.99",
+        "images": [
+          "https://example.com/sony_tv_image1.jpg",
+          "https://example.com/sony_tv_image2.jpg"
+        ],
+        "link": "https://example.com/sony_smart_tv"
+      },
+      {
+        "title": "Sony Noise-Canceling Headphones",
+        "price": "$149.99",
+        "images": [
+          "https://example.com/sony_headphones_image1.jpg",
+          "https://example.com/sony_headphones_image2.jpg"
+        ],
+        "link": "https://example.com/sony_headphones"
+      },
+      {
+        "title": "Sony PlayStation 5",
+        "price": "$499.99",
+        "images": [
+          "https://example.com/sony_ps5_image1.jpg",
+          "https://example.com/sony_ps5_image2.jpg"
+        ],
+        "link": "https://example.com/sony_ps5"
+      }
+      // Add more Sony products as needed
+    ];
+  
